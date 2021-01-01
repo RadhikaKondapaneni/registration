4bbac5a7427f63d2357aa15b9c34617cc66135ff
RadhikaKondapaneni <66896654+RadhikaKondapaneni@users.noreply.github.com>
2021-01-01 16:31:36 +0530

    my sql script

--- sql/script.sql
+++ sql/script.sql
@@ -1,1 +1,6 @@
+create database rk;
+use rk;
+create table students(ID int,fname varchar(50),lname varchar(50),Phno varchar(10),branch varchar(50),email varchar(50));
+insert into students values(17,'radhika','kondapaneni','9491674022','cse','kondapaneniradhika@gmail.com');
+insert into students values(18,'radhika','karthi','12','abcd','abcdgfgrh@gmail.com');
 
